cb363c9efbb064f7f141fd7da95896979b2c6a70
AamnaMajid <aamna.majid@retailo.co>
2021-10-08 12:45:53 +0500

    first challenge

---
+++ challenge1.js
@@ -0,0 +1,18 @@
+/* Game character challenge
+
+Create an object using bracket and dot notation that represents the characters and related data you 
+may find in a game of clue 
+
+we have weapons, murderer and characters
+
+*/
+
+
+var clueGame = {}
+
+clueGame["murderer"] = "unknown"
+
+clueGame.weapons = ['rifle', 'pistol', 'dagger']
+
+clueGame.names = ['mrs white', 'mr white']
+
